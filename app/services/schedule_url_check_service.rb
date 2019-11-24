8e3ebefd1aa35c559874d1e7b9717f07029a4855
Paul Nikitochkin <pftg@users.noreply.github.com>
2019-11-24 12:16:23 +0200

    Change strategy to not pre-cereate url before job

--- app/services/schedule_url_check_service.rb
+++ app/services/schedule_url_check_service.rb
@@ -10,7 +10,6 @@ class ScheduleUrlCheckService
   WORKER_NAME = 'Broolik::Worker::URLChecker'
 
   def perform
-    url = Url.create!(url: @url, processed: false)
-    Sidekiq::Client.push('class' => WORKER_NAME, 'args' => [url.id], 'queue' => 'worker.cr')
+    Sidekiq::Client.push('class' => WORKER_NAME, 'args' => [@url], 'queue' => 'worker.cr')
   end
 end
